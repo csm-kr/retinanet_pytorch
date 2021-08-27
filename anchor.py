92dd7599d2adba5ea733272a017d694db00b4546
csm-kr <csm8167@naver.com>
2021-08-27 11:08:41 +0900

    add anchor visualization

--- anchor.py
+++ anchor.py
@@ -1,9 +1,7 @@
 import numpy as np
 import torch
-from math import sqrt
 from abc import ABCMeta, abstractmethod
 from config import device
-from collections import OrderedDict
 from utils import cxcy_to_xy, xy_to_cxcy
 
 
@@ -39,7 +37,6 @@ class RETINA_Anchor(Anchor):
         # original paper areas
         # https://arxiv.org/abs/1708.02002 chapter 4's anchor
         areas = [32, 64, 128, 256, 512]
-        # strides = [2 ** x for x in pyramid_levels]
 
         center_anchors = []
         for f_map, area, stride in zip(feature_maps, areas, strides):
@@ -48,6 +45,7 @@ class RETINA_Anchor(Anchor):
                     c_x = (j + 0.5) / f_map
                     c_y = (i + 0.5) / f_map
 
+                    # if consider
                     # refer to https://github.com/yhenon/pytorch-retinanet
                     # c_x = (j + 0.5) * stride / img_size
                     # c_y = (i + 0.5) * stride / img_size
@@ -66,36 +64,47 @@ class RETINA_Anchor(Anchor):
         center_anchors = np.array(center_anchors).astype(np.float32)
         center_anchors = torch.FloatTensor(center_anchors).to(device)
 
-        # # center anchor clamp 방식!
-        # corner_anchors = cxcy_to_xy(center_anchors).clamp(0, 1)
-        # center_anchors = xy_to_cxcy(corner_anchors)
-        #
-        # # from matplotlib.patches import Rectangle
-        # # import matplotlib.pyplot as plt
-        # #
-        # # size = 300
-        # # img = torch.ones([size, size, 3], dtype=torch.float32)
-        # #
-        # # plt.imshow(img)
-        # # for anchor in corner_anchors:
-        # #     x1 = anchor[0] * size
-        # #     y1 = anchor[1] * size
-        # #     x2 = anchor[2] * size
-        # #     y2 = anchor[3] * size
-        # #
-        # #     plt.gca().add_patch(Rectangle(xy=(x1, y1),
-        # #                                   width=x2 - x1,
-        # #                                   height=y2 - y1,
-        # #                                   linewidth=1,
-        # #                                   ))
-        # # plt.show()
+        visualization = False
+        if visualization:
+
+            # original
+            corner_anchors = cxcy_to_xy(center_anchors)
+
+            # center anchor clamp 방식!
+            corner_anchors = cxcy_to_xy(center_anchors).clamp(0, 1)
+            center_anchors = xy_to_cxcy(corner_anchors)
+
+            from matplotlib.patches import Rectangle
+            import matplotlib.pyplot as plt
+
+            size = 300
+            img = torch.ones([size, size, 3], dtype=torch.float32)
+
+            plt.imshow(img)
+            axes = plt.axes()
+            axes.set_xlim([- 1/3 * size, size])
+            axes.set_ylim([- 1/3 * size, size])
+
+            for anchor in corner_anchors[:10]:
+                x1 = anchor[0] * size
+                y1 = anchor[1] * size
+                x2 = anchor[2] * size
+                y2 = anchor[3] * size
+
+                plt.gca().add_patch(Rectangle(xy=(x1, y1),
+                                              width=x2 - x1,
+                                              height=y2 - y1,
+                                              linewidth=1,
+                                              edgecolor=[0, 1, 0],
+                                              facecolor='none'
+                                              ))
+            plt.show()
 
         return center_anchors
 
 
 if __name__ == '__main__':
     retina_anchor = RETINA_Anchor(model_name='retina')
-
     anchor = retina_anchor.create_anchors(img_size=600)
     # corner_anchor = cxcy_to_xy(anchor) * 600
     # print(corner_anchor[:100, :])
